c01ff91080591efd78b7be13ab26a0b66870773c
Yogev Arye <106072953+YogevArye@users.noreply.github.com>
2024-06-05 16:51:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-/*
-Implementation of a function that is given 2 positive integers representing 2 angles of a triangle. If the triangle is right, isosceles, or both, the function should return 1, 2 or 3, respectively. Otherwise, it shoud return 0.
 
 
 #include <stdio.h>
