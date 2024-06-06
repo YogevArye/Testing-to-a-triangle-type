d531857d65007233847d5a8e444298cef23f1321
Yogev Arye <106072953+YogevArye@users.noreply.github.com>
2024-06-06 21:14:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int triangle_type(unsigned angle1, unsigned angle2){
-  const int right_angle = 90;
-  const unsigned angle3 = 180 - angle1 - angle2;
+int TriangleType (unsigned angle1, unsigned angle2){
+  const int RIGHT_ANGLE = 90;
+  const unsigned ANGLE_3 = 180 - angle1 - angle2;
       /*Invalid angles*/                                                                                       
-   if (angle1 + angle2 >= 180 ||angle1 + angle3 >= 180 || angle2 + angle3 >= 180 ){return -1;}
+   if (angle1 + angle2 >= 180 ||angle1 + ANGLE_3 >= 180 || angle2 + ANGLE_3 >= 180 ){return -1;}
       /*A right-angled and isosceles triangle*/                                                                                       
-  else if ((angle1 == right_angle && angle2 == right_angle-angle2)||(angle2 == right_angle && angle1 == right_angle-angle1)||(angle3 ==right_angle&&angle1==angle2)){return 3;}
+  else if ((angle1 == RIGHT_ANGLE && angle2 == RIGHT_ANGLE-angle2)||(angle2 == RIGHT_ANGLE && angle1 == RIGHT_ANGLE-angle1)||(ANGLE_3 ==RIGHT_ANGLE&&angle1==angle2)){return 3;}
         /*A isosceles triangle*/                                                                                       
-  else if (angle1 == angle2 || angle1 == angle3|| angle2 == angle3){return 2;}
+  else if (angle1 == angle2 || angle1 == ANGLE_3|| angle2 == ANGLE_3){return 2;}
       
           /*A right-angled triangle*/                                                                                       
-  else if (angle1 == right_angle || angle2 == right_angle || angle3 == right_angle){return 1;}
+  else if (angle1 == RIGHT_ANGLE || angle2 == RIGHT_ANGLE || ANGLE_3 == RIGHT_ANGLE){return 1;}
                                                                                         
                                                                                           
   else {return 0;}
@@ -22,6 +22,6 @@ int main()
 {
     unsigned a = 90;
     unsigned b = 60;
-    printf("Your triangle type is %d\n", triangle_type(a, b));
+    printf("Your triangle type is %d\n", TriangleType (a, b));
     return 0;
 }
