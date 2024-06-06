a52c71a711f086385fea19cf04771ba5aff67e02
Yogev Arye <106072953+YogevArye@users.noreply.github.com>
2024-06-06 19:02:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
 int TriangleType(unsigned angle1, unsigned angle2){
-  const int rightAngle = 90;
+  const int right_angle = 90;
   const unsigned angle3 = 180 - angle1 - angle2;
       /*Invalid angles*/                                                                                       
    if (angle1 + angle2 >= 180 ||angle1 + angle3 >= 180 || angle2 + angle3 >= 180 ){return -1;}
       /*A right-angled and isosceles triangle*/                                                                                       
-  else if ((angle1 == rightAngle && angle2 == rightAngle-angle2)||(angle2 == rightAngle && angle1 == rightAngle-angle1)||(angle3 ==rightAngle&&angle1==angle2)){return 3;}
+  else if ((angle1 == right_angle && angle2 == right_angle-angle2)||(angle2 == right_angle && angle1 == right_angle-angle1)||(angle3 ==right_angle&&angle1==angle2)){return 3;}
         /*A isosceles triangle*/                                                                                       
-  else if (angle1 == angle2 || angle1 == angle3|| angle2 == angle3){
-      return 2;}
+  else if (angle1 == angle2 || angle1 == angle3|| angle2 == angle3){return 2;}
+      
           /*A right-angled triangle*/                                                                                       
-  else if (angle1 == rightAngle || angle2 == rightAngle || angle3 == rightAngle){ return 1;}
+  else if (angle1 == right_angle || angle2 == right_angle || angle3 == right_angle){return 1;}
                                                                                         
                                                                                           
   else {return 0;}
       
 }
 
-
 int main()
 {
-    unsigned a = 60;
+    unsigned a = 90;
     unsigned b = 60;
     printf("Your triangle type is %d\n", TriangleType(a, b));
     return 0;
